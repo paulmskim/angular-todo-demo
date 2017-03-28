ff4e40bb1583de4928b31acc4fbfa1bea452e3c7
paulmskim <kim.paul.ms@gmail.com>
2017-03-29 01:26:12 +0800

    setup babel, eslint, and webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var debug = process.env.NODE_ENV !== 'production';
+var webpack = require('webpack');
+
+module.exports = {
+  entry: __dirname + '/src/js/app.module.js',
+  output: {
+    path: __dirname + '/dist/js',
+    filename: 'index.min.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+  },
+  devtool: debug ? 'cheap-module-source-map' : 'eval',
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015'],
+              plugins: ['transform-object-rest-spread'],
+            },
+          },
+        ],
+      }, {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+              removeComments: false,
+              collapseWhitespace: false,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
